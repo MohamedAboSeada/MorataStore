35675c79e49bda81c57efeea90deeeae713a462f
MohamedAboSeada <madoaxel287@gamil.com>
2024-08-26 09:40:27 +0300

    Updates

---
+++ assets/LazyImage-DBdLKvop.js
@@ -0,0 +1,1 @@
+import{r as a,j as m}from"./index-CrhDUtrK.js";const d=({ref:s,src:e,alt:o})=>{const[r,n]=a.useState(!1);return a.useEffect(()=>{const t=new Image;t.src=e,t.onload=()=>n(!0)},[e]),r?m.jsx("img",{ref:s,src:e,alt:o}):null};export{d as default};
